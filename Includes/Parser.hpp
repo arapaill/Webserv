a626fd86035630a4800cb54c3d56a4f78f30edd1
arapaill <arapaill@student.s19.be>
2022-05-05 19:05:17 +0200

    start parser

--- Includes/Parser.hpp
+++ Includes/Parser.hpp
@@ -1,12 +1,19 @@
 #ifndef PARSER_HPP
 # define PARSER_HPP
 
+# include "Config.hpp"
+
 class Parser
 {
     public:
+        Parser(void);
+		Parser(Parser const & src);
+		~Parser(void);
+
+        Parser & operator=(const Parser &rhs);
 
     private:
-    
-}
+        Config  config_file;
+};
 
 #endif
