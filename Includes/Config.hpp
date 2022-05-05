30b512b6415b795eb1290f34f90af6548c265d49
arapaill <arapaill@student.s19.be>
2022-05-05 17:50:42 +0200

    start config

---
+++ Includes/Config.hpp
@@ -0,0 +1,36 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+class Config
+{
+	public:
+		Config() : _network(NULL), _root(""), _server_name(""), _index("") {};
+		Config(Config const &other) {*this = other;};
+		~Config() {};
+
+		Config & operator=(const Config & rhs)
+		{
+			_network = rhs._network;
+			_root = rhs._root;
+			_server_name = rhs._server_name;
+			_index = rhs._index;
+		}
+
+		void		set_network(t_network &network) {_network = network;};
+		void		set_root(std::string &root) {_root = root;};
+		void		set_server_name(std::string server_name) {_server_name = server_name;};
+		void		set_index(std::string index) {_index = index;};
+
+		t_network	&get_network(void){return(_network);};
+		std::string	&get_root(void) {return(_root);};
+		std::string	&get_server_name(void){return(_server_name);};
+		std::string	&get_index(void) {return(_index);};
+
+	private:
+		t_network			_network;
+		std::string         _root;
+		std::string         _server_name;
+		std::string			_index;
+};
+
+#endif
